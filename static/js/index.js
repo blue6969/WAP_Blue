4905a1d3f3798f3fb202d12c1daf70e5736fd09a
357641320@qq.com <abc#12345>
2015-09-29 17:04:52 +0800

    fixed style

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,20 @@ $(function()
 {	   
 	$('.downapp').click(function(){
 		window.location.href='http://www.blue69.cn/download.html';
-	});
-   
+	});	
+	var userAgent = navigator.userAgent.toLowerCase(); 
+	//alert(userAgent)
 	
+	var ch=$('.content').height();
+	var wh2=500;//$(window).height()*2;
+	if(ch > wh2)
+	{
+		$('.content').height(wh2);
+		
+		$('.content-all').css("display","");
+		$('.content-all').on('click',function(){
+			$('.content').height('auto');
+			$('.content-all').css("display","none");
+			});
+	}
 });
