fac70fb34b5632e3bc284f701c04021e66e8a9f9
357641320@qq.com <abc#12345>
2015-09-30 09:54:42 +0800

    fixed style

--- static/js/index.js
+++ static/js/index.js
@@ -2,20 +2,19 @@ $(function()
 {	   
 	$('.downapp').click(function(){
 		window.location.href='http://www.blue69.cn/download.html';
-	});	
-	var userAgent = navigator.userAgent.toLowerCase(); 
-	//alert(userAgent)
-	
-	var ch=$('.content').height();
-	var wh2=500;//$(window).height()*2;
-	if(ch > wh2)
-	{
-		$('.content').height(wh2);
-		
-		$('.content-all').css("display","");
-		$('.content-all').on('click',function(){
-			$('.content').height('auto');
-			$('.content-all').css("display","none");
+	});
+	var user_agent = navigator.userAgent.toLowerCase();
+	if (user_agent.indexOf('mobile') > -1 || user_agent.indexOf('applewebkit') > -1) {
+		var ch = $('.content').height();
+		var wh2 = $(window).height() * 2;
+		if (ch > wh2) {
+			$('.content').height(wh2);
+
+			$('.content-all').css("display", "");
+			$('.content-all').on('click', function () {
+				$('.content').height('auto');
+				$('.content-all').css("display", "none");
 			});
+		}
 	}
 });
